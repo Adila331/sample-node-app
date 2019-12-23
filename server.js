7379186fb56dc8b9c8a1a281a8c4cbcb94fe191f
Adila331 <48599987+Adila331@users.noreply.github.com>
2019-12-23 09:30:08 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
 app.get("/",function(req,res){
-  res.json("Success");
+  res.json("Success my github repo");
 });
 app.listen(process.env.PORT || 8080);
